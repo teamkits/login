41957ac900000fed7298012d6d720ff2226c7d16
wangwii <wangwii@foxmail.com>
2022-12-20 19:56:22 +0800

    remove unused codes.

--- bin/tag-it.sh
+++ bin/tag-it.sh
@@ -2,7 +2,7 @@
 
 cd $(dirname $0)/..
 
-# tag_name="release-v$(date +'%Y%m%d%H%M%S')"
-tag_name="v.$(date +'%Y%m%d%H%M%S')"
+tag_name="release-v$(date +'%Y%m%d%H%M%S')"
+# tag_name="v.$(date +'%Y%m%d%H%M%S')"
 git tag "$tag_name"
 git push origin "$tag_name"
